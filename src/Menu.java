6ab24af38dd1c12130ada385b888b901d0e221e5
luisc <lcolayam@udistrital.edu.co>
2025-11-09 16:40:34 -0500

    termine el menu

---
+++ src/Menu.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Menu {
+    private JButton ahorcadoButton;
+    private JButton parejasLocasButton;
+    private JButton trikiButton;
+    private JButton calculadoraButton;
+    public void Triqui(){
+
+    }
+}
